6c9a307d66ccb6ac5299f4d60c02e62731099db6
CodingRonin <CodingRonin@bitcoin2.network>
2018-05-23 17:47:43 -0500

    Adjust Difficulty

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -465,7 +465,7 @@ Difficulty Currency::nextDifficulty(uint8_t version, uint32_t blockIndex, std::v
 if (blockIndex >= 2 && blockIndex < (UPGRADE_HEIGHT_V5-1)) {return 1500;}
 
 // easy diff for transition
-if (blockIndex >= (UPGRADE_HEIGHT_V5-1) && blockIndex <= (UPGRADE_HEIGHT_V5+100) ) {return 4000000;}
+if (blockIndex >= (UPGRADE_HEIGHT_V5-1) && blockIndex <= (UPGRADE_HEIGHT_V5+1000) ) {return 4000;}
 
 //new bitcoin2network diff code
   if (version == BLOCK_MAJOR_VERSION_5 ) {
